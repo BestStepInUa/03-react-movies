05f60fb3d5ef074aa11338daab0ac0aa727b7aa0
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-03-24 14:58:38 +0200

    react-loader-spinner added

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,17 @@
+import { FallingLines } from 'react-loader-spinner'
 import css from './Loader.module.css'
 
 export default function Loader() {
-	return <p className={css.text}>Loading movies, please wait...</p>
+	return (
+		<div className={css.wrapper}>
+			<FallingLines
+				color='#084298'
+				width='100'
+				visible={true}
+				ariaLabel='falling-circles-loading'
+			/>
+			<p className={css.text}>Loading movies, please wait...</p>
+		</div>
+	)
 }
 
